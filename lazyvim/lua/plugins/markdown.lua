d967084f73015f53ef752c5a88a7aa3b59ea51fb
cy.Y <chenyang.yan@qlchat.com>
2026-06-03 17:26:36 +0800

    Disable Markdown lint diagnostics in LazyVim

--- lazyvim/lua/plugins/markdown.lua
+++ lazyvim/lua/plugins/markdown.lua
@@ -1,16 +1,19 @@
 return {
   {
     "mfussenegger/nvim-lint",
-    opts = {
-      linters = {
-        ["markdownlint-cli2"] = {
-          args = {
-            "--config",
-            vim.fn.stdpath("config") .. "/.markdownlint.json",
-            "-",
-          },
-        },
-      },
-    },
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.markdown = {}
+      opts.linters_by_ft["markdown.mdx"] = {}
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.sources = vim.tbl_filter(function(source)
+        return source.name ~= "markdownlint_cli2" and source.name ~= "markdownlint"
+      end, opts.sources or {})
+    end,
   },
 }
